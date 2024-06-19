ad5903919b065a232258444e8ed552b95cd49275
WebWithRathor <devrajrathor143@gmail.com>
2024-06-19 15:55:59 +0530

    FakeApiStart

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Nav from './components/Nav'
+
+function App() {
+
+  return (
+    <>
+    <div className="main h-screen w-full flex">
+      <Nav/>
+    </div>
+    </>
+  )
+  }
+export default App
